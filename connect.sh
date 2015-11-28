db1945bf3ebd2745753d8125e35947c2e38ac871
scoiatael <czapl.luk@gmail.com>
2015-11-28 17:57:44 +0100

    improved connect script

--- connect.sh
+++ connect.sh
@@ -3,12 +3,22 @@ PORT=31338
 USERNAME='i247926'
 SSH_FLAGS="-A -t -L $PORT:localhost:$PORT"
 
-HOPS=""
+HOPS=(
+"$USERNAME@tryglaw.ii.uni.wroc.pl"
+"szewc@evolution.ii.uni.wroc.pl"
+"-p 2222 $USERNAME@localhost"
+"$USERNAME@lab110-02"
+)
 
-CMD="ssh $SSH_FLAGS szewc@evolution.ii.uni.wroc.pl ssh $SSH_FLAGS -p 2222 $USERNAME@localhost "
+CMD=""
 
-for hop in $HOPS; do
-  CMD="$CMD ssh $SSH_FLAGS $USERNAME@$hop"
+# get length of an array
+tLen=${#HOPS[@]}
+
+# use for loop read all hops
+for (( i=0; i<${tLen}; i++ ));
+do
+  CMD="$CMD ssh $SSH_FLAGS ${HOPS[$i]}"
 done
 
 CMD="$CMD $@"
