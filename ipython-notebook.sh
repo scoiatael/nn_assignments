2a4d66cf0098950d4aab000bc192c58a7cc85afb
Scoiatael <czapl.luk@gmail.com>
2016-01-24 16:32:21 +0100

    GPU and attempt to learn bigger net

--- ipython-notebook.sh
+++ ipython-notebook.sh
@@ -4,5 +4,9 @@
 export NN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source "$NN/set-env.sh"
+PIO_CUDA="/pio/os/cuda-7.0/set-env.sh"
+[ -f $PIO_CUDA ] && source $PIO_CUDA && echo 'Sourced cuda configuration'
+
+export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
 
 exec ipython notebook --profile=foo
